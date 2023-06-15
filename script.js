9603e7d5f00192ef08e2313d6f7f7eda6b9a8f17
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-06-15 23:40:31 +0500

    Do JS Practice

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
-var array = []
+// var array = []
 
-var obj = {
-    name: "Muzammil-Husnain",
-    rollNo: 569453,
-    degree: "bscs"
-}
-array.push(obj)
+// var obj = {
+//     name: "Muzammil-Husnain",
+//     rollNo: 569453,
+//     degree: "bscs"
+// }
+// array.push(obj)
 
 
+//  obj = {
+//     name: "Shehraz-Hassam",
+//     rollNo: 569453,
+//     degree: "bscs"
+// }
+// array.push(obj)
 
- obj = {
-    name: "Shehraz-Hassam",
-    rollNo: 569453,
-    degree: "bscs"
-}
-array.push(obj)
+//  obj = {
+//     name: "Imran-Khan",
+//     rollNo: 569453,
+//     degree: "bscs"
+// }
 
- obj = {
-    name: "Imran-Khan",
-    rollNo: 569453,
-    degree: "bscs"
-}
-array.push(obj)
+// array.push(obj)
 
-console.log(array);
+// var name = "ali";
+// var rollNo = 569454
+
+// obj.name = name
+// obj.rollNo = rollNo
+
+// console.log(array);
+
+
+// var fruits = ['apple', 'banana', 'orange', 'mango', 'grape'];
+
+// var slicedFruits = fruits.slice(1, 4);
+// console.log(slicedFruits);
+// // Output: ['banana', 'orange', 'mango']
+
+// var slicedFruitsFromStart = fruits.slice(2);
+// console.log(slicedFruitsFromStart);
+// // Output: ['orange', 'mango', 'grape']
+
+var fruits = ['apple', 'banana', 'orange', 'mango', 'grape'];
+
+var slicedFruits = fruits.slice(0 , 3)
+
+console.log(slicedFruits);
