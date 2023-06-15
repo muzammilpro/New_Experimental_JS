3de4fad7d8b3d31b16ed99d9ea7b91bc6b5497d9
Muzammil Husnain <muzammilhusnain7@gmail.com>
2023-06-15 23:02:38 +0500

    Experimental JS Started

---
+++ script.js
@@ -0,0 +1,26 @@
+var array = []
+
+var obj = {
+    name: "Muzammil-Husnain",
+    rollNo: 569453,
+    degree: "bscs"
+}
+array.push(obj)
+
+
+
+ obj = {
+    name: "Shehraz-Hassam",
+    rollNo: 569453,
+    degree: "bscs"
+}
+array.push(obj)
+
+ obj = {
+    name: "Imran-Khan",
+    rollNo: 569453,
+    degree: "bscs"
+}
+array.push(obj)
+
+console.log(array);
